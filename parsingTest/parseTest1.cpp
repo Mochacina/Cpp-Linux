69aec4d0b9849eef587d0841e3f21781266ead16
mochacina <nayuta5554@gmail.com>
2024-01-25 10:32:17 +0900

    변경사항 반영

--- parsingTest/parseTest1.cpp
+++ parsingTest/parseTest1.cpp
@@ -3,6 +3,15 @@
 #include <vector>
 #include <sstream>
 
+using namespace std;
+
 int main(){
-    
+    string input;
+    getline(cin, input);
+    istringstream is(input);
+    string token;
+
+    while(getline(is,token,',')){
+        cout << "token: " << token << endl;
+    }
 }
