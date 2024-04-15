a82e7a558e6e6f12672a469dfcada959b9f3c307
mochacina <nayuta5554@gmail.com>
2024-04-15 18:31:05 +0900

    commit

--- barkingDog/0x03_1.cpp
+++ barkingDog/0x03_1.cpp
@@ -13,7 +13,7 @@ int func2(int arr[], int N){
 }
 
 int main(){
-    //ios::sync_with_stdio(0);cin.tie(0);
+    ios::sync_with_stdio(0);cin.tie(0);
     
     int size;
     cout << "Input array size: ";
