bb9d2006570654e169019a8b0f833f470e693121
mochacina <nayuta5554@gmail.com>
2024-04-25 15:05:48 +0900

    test commit

--- oopTest/inherit2.cpp
+++ oopTest/inherit2.cpp
@@ -29,22 +29,28 @@ public:
 };
 
 int main() {
-    Parent* p = new Child();  // Parent 타입의 포인터로 Child 객체 생성
-    p->show();  // 다형성을 통해 Child의 show 함수 호출
-    p->parentOnly();  // Parent 클래스의 parentOnly 함수 호출
-    try {
-        p->childOnly();  // Parent 클래스에는 childOnly 함수가 없으므로 호출 불가능
-    } 
-    catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
-    }
+    Parent* p = new Parent();
+    p->show();
+    p->parentOnly();
+
+    std::cout << "---------\n";
+
+    Parent* pc = new Child();  // Parent 타입의 포인터로 Child 객체 생성
+    pc->show();  // 다형성을 통해 Child의 show 함수 호출
+    pc->parentOnly();  // Parent 클래스의 parentOnly 함수 호출
+    //pc->childOnly();  // Parent 클래스에는 childOnly 함수가 없으므로 호출 불가능
+
+    std::cout << "---------\n";
 
     Child* c = new Child;  // Child 객체 생성
     c->show();  // Child 클래스의 show 함수 호출
     c->parentOnly();  // Parent 클래스의 parentOnly 함수 호출
     c->childOnly();  // Child 클래스의 childOnly 함수 호출
 
-    delete p;  // 할당된 메모리 해제, Child의 소멸자를 호출한 후 Parent의 소멸자 호출
-    delete c;  // 할당된 메모리 해제, Child의 소멸자만 호출
+    std::cout << "---------\n";
+
+    delete p;   // 할당된 메모리 해제, Parent의 소멸자 호출
+    delete pc;  // 할당된 메모리 해제, Child의 소멸자를 호출한 후 Parent의 소멸자 호출
+    delete c;  // 할당된 메모리 해제, Child의 소멸자를 호출한 후 Parent의 소멸자 호출
     return 0;
 }
