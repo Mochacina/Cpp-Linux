214a20ab1859146c60cdd53125c6f78503a364a7
mochacina <nayuta5554@gmail.com>
2024-08-09 18:11:56 +0900

    commit

---
+++ structTest/structTest.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct
+{
+    int dataInt = 0;
+    string dataStr = "test";
+} test1;
+
+int main(){
+    test1 st;
+    cout << &st << endl;
+
+}
